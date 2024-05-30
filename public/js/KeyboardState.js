53d536bc29e2bf3f728ba9338d673f67f791034b
kevenW1lian <100875547+kevenW1lian@users.noreply.github.com>
2024-05-30 20:15:45 -0300

    Video 11

--- public/js/KeyboardState.js
+++ public/js/KeyboardState.js
@@ -3,10 +3,8 @@ const RELEASED = 0;
 
 export default class KeyboardState {
     constructor() {
-        // Holds the current state of a given key
         this.keyStates = new Map();
 
-        // Holds the callback functions for a key code
         this.keyMap = new Map();
     }
 
@@ -18,7 +16,6 @@ export default class KeyboardState {
         const {code} = event;
 
         if (!this.keyMap.has(code)) {
-            // Did not have key mapped.
             return;
         }
 
