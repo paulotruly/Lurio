28aa3cda35b8ef1a9cb7fd24534426134412181b
Pedro Tavares <pedrotavaresgf@gmail.com>
2024-05-30 22:38:54 -0300

    Ep 12 - Entity Interactions - interações e colisões entre entidades

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,20 @@
 import Camera from './Camera.js';
+import Entity from './Entity.js';
+import PlayerController from './traits/PlayerController.js';
 import Timer from './Timer.js';
 import {createLevelLoader} from './loaders/level.js';
 import {loadEntities} from './entities.js';
 import {setupKeyboard} from './input.js';
 import {createCollisionLayer} from './layers.js';
+
+function createPlayerEnv(playerEntity) {
+    const playerEnv = new Entity();
+    const playerControl = new PlayerController();
+    playerControl.checkpoint.set(64, 64);
+    playerControl.setPlayer(playerEntity);
+    playerEnv.addTrait(playerControl);
+    return playerEnv;
+}
 
 async function main(canvas) {
     const context = canvas.getContext('2d');
@@ -16,9 +27,10 @@ async function main(canvas) {
     const camera = new Camera();
 
     const mario = entityFactory.mario();
-    mario.pos.set(64, 64);
 
-    level.entities.add(mario);
+    const playerEnv = createPlayerEnv(mario);
+    level.entities.add(playerEnv);
+
 
     level.comp.layers.push(createCollisionLayer(level));
 
@@ -29,9 +41,7 @@ async function main(canvas) {
     timer.update = function update(deltaTime) {
         level.update(deltaTime);
 
-        if (mario.pos.x > 100) {
-            camera.pos.x = mario.pos.x - 100;
-        }
+        camera.pos.x = Math.max(0, mario.pos.x - 100);
 
         level.comp.draw(context, camera);
     }
