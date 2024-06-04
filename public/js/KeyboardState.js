5f8c5f74ce517b125a828e1258984155ca76b805
JoaoBoudoux <demarcialima@hotmail.com>
2024-06-03 21:05:18 -0300

    Video 21

--- public/js/KeyboardState.js
+++ public/js/KeyboardState.js
@@ -3,10 +3,10 @@ const RELEASED = 0;
 
 export default class KeyboardState {
     constructor() {
-        
+        // Holds the current state of a given key
         this.keyStates = new Map();
 
-        
+        // Holds the callback functions for a key code
         this.keyMap = new Map();
     }
 
--- public/js/main.js
+++ public/js/main.js
@@ -8,7 +8,6 @@ import {setupKeyboard} from './input.js';
 import {createCollisionLayer} from './layers/collision.js';
 import {createDashboardLayer} from './layers/dashboard.js';
 
-
 async function main(canvas) {
     const context = canvas.getContext('2d');
     const audioContext = new AudioContext();
@@ -26,13 +25,14 @@ async function main(canvas) {
     const camera = new Camera();
 
     const mario = createPlayer(entityFactory.mario());
+    mario.player.name = "MARIO";
+    level.entities.add(mario);
 
     const playerEnv = createPlayerEnv(mario);
     level.entities.add(playerEnv);
 
-
     level.comp.layers.push(createCollisionLayer(level));
-    level.comp.layers.push(createDashboardLayer(font, playerEnv));
+    level.comp.layers.push(createDashboardLayer(font, level));
 
     const input = setupKeyboard(mario);
     input.listenTo(window);
@@ -54,7 +54,6 @@ async function main(canvas) {
     }
 
     timer.start();
-    level.music.player.playTrack('main');
 }
 
 const canvas = document.getElementById('screen');
